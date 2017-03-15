77a3fa110e132f4b7e3041e617937ad9fa198e43
Shadi Khaled <shadikhaled777@gmail.com>
2017-03-15 12:23:55 +0300

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+	for (var i = 0; i < 25; i++) {
+		if(i===1){
+		array.push(`I am 1 strange loops.`)}else{
+			array.push(`I am ${i} strange loops.`)
+		}
+	}
+	return array// body...
+}
+function whileLoop(num){
+	while(num>0){
+		console.log(num--)
+	}
+	return 'done';
+}
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
